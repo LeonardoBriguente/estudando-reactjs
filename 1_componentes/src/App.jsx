da9bdad0dc37a8a053f116981416bfdd16954547
LeonardoBriguente <leobriguente27@gmail.com>
2024-12-20 21:56:27 -0300

    adiciona componente PropsExample e o utiliza no App

--- 1_componentes/src/App.jsx
+++ 1_componentes/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 // importar componente filho
 import FuncionalComponent from "./components/FunctionalComponent";
 import ClassComponent from "./components/ClassComponent";
+import PropsExample from "./components/PropsExample";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       {/* Utilizando componente importado no JSX */}
       <FuncionalComponent />
       <ClassComponent />
+      <PropsExample nome="Leonardo" idade={21} />
     </>
   )
 }
