815bb7a71c872b3699ab906a41ccaad56eff4885
LeonardoBriguente <leobriguente27@gmail.com>
2024-12-21 13:00:11 -0300

    Finalizado aula sobre JSX e aplicações nele

--- 2_jsx/src/components/JsxExamples.jsx
+++ 2_jsx/src/components/JsxExamples.jsx
@@ -16,8 +16,30 @@ const JsxExamples = () => {
         return <p>Olá, visitante</p>
     }
 
+    const userIsLogged = true;
+    const userRole = 'admin';
+
+    const users = [
+        {
+            id: 1,
+            name: 'John Doe',
+            age: 25
+        },
+        {
+            id: 2,
+            name: 'Jane Doe',
+            age: 30
+        },
+        {
+            id: 3,
+            name: 'Leonidas Esteban',
+            age: 35
+        }	
+    ]
+
   return (
     <div>
+        {/* BÁSICO */}
         <h2>Conteúdo que o usuário vai ver</h2>
         <p>Olá, {name}</p>
         <p>Usuário: {user.firstName} {user.lastName}, {user.age} anos</p>
@@ -25,6 +47,29 @@ const JsxExamples = () => {
         <p>{getGreetings(name)}</p>
         <p>{getGreetings()}</p>
         {/* <p>{getGreetings("Leonidas")}</p> */}
+
+        {/* DIFERENÇAS DO HTML */}
+        <div className="alguma-coisa"> Estilizar esse cara, por exemplo</div>
+
+        <button onClick={() => alert("Teste do onClick")}>Clique em mim</button>
+
+        <input type="text" placeholder="Digite alguma coisa" />
+
+        {/* RENDERIZAÇÃO CONDICIONAL */}
+        {userIsLogged ? <p>Caso: Está logado</p> : <p>Caso: Não está logado</p>}
+
+        <p>{userRole === "admin" && "Você é um admin"}</p>
+
+        {/* RENDERIZAÇÃO DE LISTAS */}
+        <div>
+            <ul>
+            {users.map((user) => (
+                <li key={user.id}>
+                    {user.id} - {user.name}, {user.age} anos
+                </li>
+            ))}
+            </ul>
+        </div>
     </div>
   )
 }
